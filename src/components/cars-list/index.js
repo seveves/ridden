230c620f33157177430a6f88612bbffdf59d30d8
Severin <sevefriede@gmail.com>
2018-03-21 23:11:00 +0100

    changed list style

--- src/components/cars-list/index.js
+++ src/components/cars-list/index.js
@@ -10,9 +10,20 @@ export const CarsList = ({ cars }) => (
 						cars.map((car) => (
 							<Link href={`/fleet-details/${car._id}`}>
 								<li>
-									<div>
-										<div class="list-label">{car.name}</div>
-										<div class="list-text toe">{car.description}</div>
+									<div class="d-flex flex-row aic">
+										<div>
+											<div class="list-label">{car.name}</div>
+											<div class="list-text toe">{car.description}</div>
+										</div>
+										<div class="ml-auto mr-2">
+											<div class="list-label">{car.max}</div>
+											<div class="list-text">seats</div>
+										</div>
+										<div>
+											<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+												<path fill="#666" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+											</svg>
+										</div>
 									</div>
 								</li>
 							</Link>
--- src/components/shuttles-list/index.js
+++ src/components/shuttles-list/index.js
@@ -1,14 +1,45 @@
 import { h } from 'preact';
+import { Link } from 'preact-router/match';
 
 export const ShuttlesList = ({ shuttles }) => (
 	<div>
-		{ shuttles !== null ? shuttles.map((shuttle) => (
-			<p>{shuttle.title}</p>
-		)) : null }
+		{ shuttles !== null
+			? ( 
+					<ul class="list w-100">
+					{
+						shuttles.map((shuttle) => (
+							<Link href={`/shuttle-details/${shuttle._id}`}>
+								<li>
+									<div class="d-flex flex-row aic">
+										<div>
+											<div class="list-label">{shuttle.title}</div>
+											<div class="list-text toe">{shuttle.description}</div>
+										</div>
+										<div class="ml-auto mr-2">
+											<div class="list-label">{new Date(shuttle.departure).toLocaleString()}</div>
+											<div class="list-text">departure</div>
+										</div>
+										<div class="mr-2">
+											<div class="list-label">{shuttle.bookings.length}/{shuttle.max}</div>
+											<div class="list-text">seats taken</div>
+										</div>
+										<div>
+											<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+												<path fill="#666" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+											</svg>
+										</div>
+									</div>
+								</li>
+							</Link>
+						))
+					}
+					</ul>
+				)
+			: null }
 		{ shuttles === null
 			? <p>Loading ⏳</p>
 			: (!shuttles.length)
-				? <p>there are no shuttles at the moment 🙁</p>
+				? <p>there are no shuttles near your place 🙁</p>
 				: null }
 	</div>
 );
