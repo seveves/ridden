2f3a90a93e03be862f00c8d531a5925609035526
Severin <sevefriede@gmail.com>
2018-04-01 15:31:04 +0200

    fixed header toggle issue

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,29 +2,41 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import { bus } from '../../utils';
 
+const BURGER_OPEN = 'burger change';
+const BURGER_CLOSE = 'burger';
+const MENU_OPEN = 'toggle-vis';
+const MENU_CLOSE = 'toggle-vis hidden-sm';
+
 export default class Header extends Component {
 
+	state = {
+		menuOpen: false
+	};
+
 	toggleMenu = () => {
-    document.getElementsByClassName('burger')[0].classList.toggle('change');
-    document.getElementsByClassName('toggle-vis')[0].classList.toggle('hidden-sm');
+		this.setState({ menuOpen: !this.state.menuOpen });
 	}
 	
 	componentDidMount() {
-		bus.on('route:change', ev => this.toggleMenu());
+		bus.on('route:change', ev => {
+			if (this.state.menuOpen) {
+				this.setState({ menuOpen: false });
+			}
+		});
 	}
 
-	render({ user, logout }) {
+	render({ user, logout }, { menuOpen }) {
 		return (
 			<header>
 				<h1><a href="/">ridden</a></h1>
-				<div class="burger" onClick={this.toggleMenu}>
+				<div class={menuOpen ? BURGER_OPEN : BURGER_CLOSE } onClick={this.toggleMenu}>
           <div class="bar1"></div>
           <div class="bar2"></div>
           <div class="bar3"></div>
         </div>
 				{ user
 					? (
-						<nav class="toggle-vis hidden-sm">
+						<nav class={menuOpen ? MENU_OPEN : MENU_CLOSE}>
 							<Link activeClassName="active" href="/">shuttles</Link>
 							<Link activeClassName="active" href="/bookings">bookings</Link>
 							{ user.isVendor ? <Link activeClassName="active" href="/offers">shuttle offers</Link> : null }
@@ -33,7 +45,7 @@ export default class Header extends Component {
 						</nav>
 					)
 					: (
-						<nav class="toggle-vis hidden-sm">
+						<nav class={menuOpen ? MENU_OPEN : MENU_CLOSE}>
 							<Link activeClassName="active" href="/login">login</Link>
 						</nav>
 					)
